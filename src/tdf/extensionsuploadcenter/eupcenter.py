5443cade60036b45cf11aca2364aa47789fc3e92
Andreas Mantke <maand@gmx.de>
2014-02-20 21:50:47 +0100

    Fixing some strings and remove a double of a function.

--- src/tdf/extensionsuploadcenter/eupcenter.py
+++ src/tdf/extensionsuploadcenter/eupcenter.py
@@ -219,14 +219,3 @@ class View(dexterity.DisplayForm):
 
         return self.catalog(**contentFilter)
 
-
-
-    def get_latest_program_release(self):
-        """Get the latest version from the vocabulary. This only
-        goes by string sorting so would need to be reworked if the
-        plone versions dramatically changed"""
-        # getAvailableVersions is coming from root.py. ?
-
-        versions = list(self.context.available_versions)
-        versions.sort(reverse=True)
-        return versions[0]
