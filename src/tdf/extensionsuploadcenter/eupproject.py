fc46f013916ef8d9ba8f329cb3d3911b30c3bfca
andreasma <maand@gmx.de>
2015-09-20 21:28:16 +0200

    Length of the project title shortened to 80 character.

--- src/tdf/extensionsuploadcenter/eupproject.py
+++ src/tdf/extensionsuploadcenter/eupproject.py
@@ -70,9 +70,9 @@ class IEUpProject(form.Schema):
     dexteritytextindexer.searchable('title')
     title = schema.TextLine(
         title=_(u"Title"),
-        description=_(u"Project Title - minimum 5 and maximum 50 characters"),
+        description=_(u"Project Title - minimum 5 and maximum 80 characters"),
         min_length=5,
-        max_length=100
+        max_length=80
     )
     
     dexteritytextindexer.searchable('description')
